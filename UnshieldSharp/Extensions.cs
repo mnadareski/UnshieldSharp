e7bd8387b5e3ad4a735c90343e55b383055ba6ce
Matt Nadareski <mnadareski@outlook.com>
2024-04-23 14:48:41 -0400

    Convert to switch expression

--- UnshieldSharp/Extensions.cs
+++ UnshieldSharp/Extensions.cs
@@ -7,46 +7,25 @@ namespace UnshieldSharp
     internal static class Extensions
     {
         /// <summary>
-        /// Read a string whose length is determined by a 2-byte header from the stream
-        /// </summary>
-        /// <param name="stream">Stream to read from</param>
-        public static string ReadUInt16HeaderedString(this Stream stream)
-        {
-            ushort len = stream.ReadUInt16();
-            byte[] buf = stream.ReadBytes(len);
-            return Encoding.ASCII.GetString(buf, 0, len);
-        }
-
-        /// <summary>
         /// Get the zlib result name from an integer
         /// </summary>
         /// <param name="result">Integer to translate to the result name</param>
         /// <returns>Name of the result, the integer as a string otherwise</returns>
         public static string ToZlibConstName(this int result)
         {
-            switch (result)
+            return result switch
             {
-                case 0:
-                    return "Z_OK";
-                case 1:
-                    return "Z_STREAM_END";
-                case 2:
-                    return "Z_NEED_DICT";
-                case -1:
-                    return "Z_ERRNO";
-                case -2:
-                    return "Z_STREAM_ERROR";
-                case -3:
-                    return "Z_DATA_ERROR";
-                case -4:
-                    return "Z_MEM_ERROR";
-                case -5:
-                    return "Z_BUF_ERROR";
-                case -6:
-                    return "Z_VERSION_ERROR";
-                default:
-                    return result.ToString();
-            }
+                0 => "Z_OK",
+                1 => "Z_STREAM_END",
+                2 => "Z_NEED_DICT",
+                -1 => "Z_ERRNO",
+                -2 => "Z_STREAM_ERROR",
+                -3 => "Z_DATA_ERROR",
+                -4 => "Z_MEM_ERROR",
+                -5 => "Z_BUF_ERROR",
+                -6 => "Z_VERSION_ERROR",
+                _ => result.ToString(),
+            };
         }
     }
 }
