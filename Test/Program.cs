bb9cd947b0f5b32d9d01ae2147c31bc9d8d9b530
Matt Nadareski <mnadareski@outlook.com>
2024-10-02 01:07:21 -0400

    Include file group name in default output path

--- Test/Program.cs
+++ Test/Program.cs
@@ -274,10 +274,12 @@ namespace Test
                     char[]? directoryChars = cab.HeaderList.GetDirectoryName((int)cab.HeaderList.GetFileDirectoryIndex(i))?.Select(c => Path.GetInvalidPathChars().Contains(c) ? '_' : c)?.ToArray();
                     string directory = directoryChars != null ? new(directoryChars) : string.Empty;
 
+                    string fileGroup = FindFileGroup(cab, i);
+
 #if NET20 || NET35
-                    string newfile = Path.Combine(Path.Combine(outputDirectory, directory), filename);
+                    string newfile = Path.Combine(Path.Combine(Path.Combine(outputDirectory, fileGroup), directory), filename);
 #else
-                    string newfile = Path.Combine(outputDirectory, directory, filename);
+                    string newfile = Path.Combine(outputDirectory, fileGroup, directory, filename);
 #endif
 
                     string? directoryName = Path.GetDirectoryName(newfile);
@@ -317,6 +319,42 @@ namespace Test
                 return directoryName;
             else
                 return Path.Combine(directoryName!, fileNameWithoutExtension!);
+        }
+    
+        /// <summary>
+        /// Find the file group for a given file index
+        /// </summary>
+        /// <param name="cab">Cabinet containing the file index</param>
+        /// <param name="fileIndex">Index of the file to check</param>
+        /// <returns>File group name on success, empty string on error</returns>
+        private static string FindFileGroup(InstallShieldCabinet? cab, int fileIndex)
+        {
+            // Handle an invalid cabinet
+            if (cab?.HeaderList == null)
+                return string.Empty;
+
+            // Handle an invalid file index
+            if (fileIndex < 0 || fileIndex > cab.HeaderList.FileCount)
+                return string.Empty;
+
+            // Search all file groups
+            for (int i = 0; i < cab.HeaderList.FileGroupCount; i++)
+            {
+                // Get the file group for the index
+                var fileGroup = cab.HeaderList.GetFileGroup(i);
+                if (fileGroup == null)
+                    continue;
+
+                // Check the range in the file group
+                if (fileGroup.FirstFile > fileIndex || fileGroup.LastFile < fileIndex)
+                    continue;
+
+                // Get and return the file group name
+                return cab.HeaderList.GetFileGroupName(i) ?? string.Empty;
+            }
+
+            // If no group was found
+            return string.Empty;
         }
     }
 }
